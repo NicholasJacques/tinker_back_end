f7bf8938757c4a0f02e24a5ebf7e8471c4025b8c
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-07-22 14:26:55 -0600

    builds user model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: /@/
+
+  before_save :downcase_email
+
+  def downcase_email
+    self.email = self.email.delete(' ').downcase
+  end
+
+end
